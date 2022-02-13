3b85dfd51e11685e0cc0a5e9ff2fa34f8a7a52e1
Gabriel Fernella <gabrielfernella07@hotmail.com>
2022-02-13 02:08:39 -0300

    Crud Cliente

--- ProgramacaoOODevSuperior/spring-boot-part2/cursomc-start2/src/main/java/com/domain/cursomc/services/ClienteService.java
+++ ProgramacaoOODevSuperior/spring-boot-part2/cursomc-start2/src/main/java/com/domain/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.domain.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.domain.cursomc.domain.Cliente;
+import com.domain.cursomc.dto.ClienteDTO;
 import com.domain.cursomc.repositories.ClienteRepository;
+import com.domain.cursomc.services.exceptions.DataIntegrityException;
 import com.domain.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +28,49 @@ public class ClienteService {
 					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Cliente porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		// Esse é um método do spingboot para fazer paginação
+		return repo.findAll(pageRequest);
+	}
+
+	// método auxiliar que instancia Cliente apartir de um DTO
+	public Cliente fromDTO(ClienteDTO objDto) {
+		//throw new UnsupportedOperationException(); //Retorna que essa função não é suportada
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	// Método utilziado para fazer um parse e atualziar apenas os campos que desejamos
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
